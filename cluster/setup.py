68515c8b85aa604545a083beeab12742c9f4b878
alexandreday <alexandre.day1@gmail.com>
2017-02-03 10:41:34 -0500

    Package organization

---
+++ cluster/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cluster',
+      version='0.1',
+      description='Clustering in 2d',
+      url='https://github.com/alexandreday/fast_density_clustering',
+      author='Alexandre Day',
+      author_email='alexandre.day1@gmail.com',
+      license='MIT',
+      packages=['cluster'],
+      zip_safe=False)
