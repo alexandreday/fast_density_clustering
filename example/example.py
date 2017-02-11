5294d1f7115d84d39c212037fa7a9f1a53ac7802
Alexandre Day <alexandre.day1@gmail.com>
2017-02-11 01:38:37 -0500

    Removed odd colors, example improved

--- example/example.py
+++ example/example.py
@@ -13,10 +13,10 @@ from sklearn.datasets import make_blobs
 from clustering import plotting
 import pickle
 
-n_true_center = 20
+n_true_center = 15
 print("------> Example with %i true cluster centers <-------"%n_true_center)
 X, y = make_blobs(10000, 2, n_true_center)
-fdc = FDC(noise_threshold=0.01, NH_size=20)
+fdc = FDC(noise_threshold=0.05, NH_size=40)
 cluster_label, idx_centers, rho, delta, kde_tree = fdc.fit(X)
 
 plotting.summary(idx_centers, cluster_label, rho, n_true_center, X, y, savefile="result.png")
