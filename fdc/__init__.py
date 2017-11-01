b87d7beb2ad3f0c92e9a5cdc72f5dd6be807cbd5
Alexandre Day <alexandre.day1@gmail.com>
2017-11-01 17:17:01 -0400

    import plotting fixed

--- fdc/__init__.py
+++ fdc/__init__.py
@@ -2,5 +2,6 @@ from .density_estimation import KDE
 from .fdc import FDC
 from .tree import TREE
 from .mycolors import COLOR_PALETTE
+from . import plotting
 
-__all__ = ['FDC', 'KDE', 'TREE', 'COLOR_PALETTE']
+__all__ = ['FDC', 'KDE', 'TREE', 'COLOR_PALETTE','plotting']
